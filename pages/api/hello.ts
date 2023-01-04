2c28df236aafdb571108c86d081bed0a87d90e36
Vincent Sjögren <vincentsjogren@gmail.com>
2023-01-04 13:07:16 +0100

    installed axios and updated example api route

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
 
-type Data = {
-  name: string;
-};
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' });
+  const { data } = await axios.get<{
+    type: string;
+    greeting: string;
+    language: string;
+  }>('https://www.greetingsapi.com/random');
+  res.status(200).json({
+    greeting: `${data.type} in ${data.language} is ${data.greeting}`,
+  });
 }
