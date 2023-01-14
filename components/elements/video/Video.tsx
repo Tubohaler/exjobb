7f5f3ff1a18740474cf18d9eb2131e6d744878da
Vincent Sjögren <vincentsjogren@gmail.com>
2023-01-14 12:03:58 +0100

    simplified Video component

--- components/elements/video/Video.tsx
+++ components/elements/video/Video.tsx
@@ -1,53 +1,38 @@
 import type { VideoFragment } from '@lib/dato-cms';
-import { Box, createStyles, Selectors, DefaultProps } from '@mantine/core';
+import { DetailedHTMLProps, VideoHTMLAttributes } from 'react';
 
+// To reduce data transfer from cms in dev mode
 const devMode = process.env.NODE_ENV === 'development';
 
-export type VideoStylesNames = Selectors<typeof useStyles>;
-export type VideoStylesParams = Pick<VideoFragment, 'width' | 'height'>;
-
-export type VideoProps = Parameters<typeof Box<'video'>>[0] &
-  DefaultProps<VideoStylesNames, VideoStylesParams> & {
-    video: VideoFragment;
-  };
-
-const useStyles = createStyles(
-  (theme, { width, height }: VideoStylesParams) => {
-    return {
-      root: {},
-    };
-  }
-);
+export type VideoProps = Omit<
+  DetailedHTMLProps<VideoHTMLAttributes<HTMLVideoElement>, HTMLVideoElement>,
+  'children'
+> & {
+  video: VideoFragment;
+  fallback?: React.ReactNode;
+};
 
 const Video = ({
   video: { width, height, mimeType, video },
-  styles,
-  unstyled,
-  className,
-  classNames,
+  fallback,
   ...props
 }: VideoProps) => {
-  const { classes, cx } = useStyles(
-    { width, height },
-    { name: 'Video', styles, unstyled, classNames }
-  );
   return (
-    <Box
-      component="video"
-      className={cx(classes.root, className)}
-      width={width || 'auto'}
-      height={height || 'auto'}
-      autoPlay
-      muted
-      playsInline
-      loop
-      {...props}
-    >
+    <video width={width || 'auto'} height={height || 'auto'} {...props}>
       <source
         src={(devMode ? video.mp4Low : video.mp4High) || ''}
         type={mimeType}
       />
-    </Box>
+      {fallback}
+    </video>
   );
 };
+
+Video.defaultProps = {
+  autoPlay: true,
+  muted: true,
+  playsInline: true,
+  loop: true,
+};
+
 export default Video;
