55905f9cb57204775ad5f3aeedc2dcdf3727622a
David Skotte <david.skotte@gmail.com>
2023-01-16 13:24:00 +0100

    projects stuff working, no styling though

---
+++ components/content/Project.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Image } from 'react-datocms';
+
+import { ProjectFragment } from '@lib/dato-cms';
+
+import { Grid } from '@mantine/core';
+
+const Project = ({ name, featuredImage }: ProjectFragment) => {
+  return (
+    <>
+      <h2>{name}</h2>
+      <Image data={featuredImage.responsiveImage} />
+    </>
+  );
+};
+
+export default Project;
