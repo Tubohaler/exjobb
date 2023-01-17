a6b4c340b3bacf4c622dde4df44a2527a52ef22d
David Skotte <david.skotte@gmail.com>
2023-01-17 11:29:20 +0100

    still working on that styling

--- components/content/ProjectGallery.tsx
+++ components/content/ProjectGallery.tsx
@@ -2,46 +2,30 @@ import React from 'react';
 
 import Project from './Project';
 
-import { Container, Flex, Grid, Text } from '@mantine/core';
+import { Box, createStyles } from '@mantine/core';
 
-import { ProjectFragment, ProjectGalleryFragment } from '@lib/dato-cms';
+import { ProjectGalleryFragment } from '@lib/dato-cms';
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(2, 1fr)',
+    alignContent: 'center',
+    justifyItems: 'center',
+
+    [theme.fn.largerThan('md')]: { gridTemplateColumns: 'repeat(4, 1fr)' },
+  },
+}));
 
 const ProjectGallery = ({ projects }: ProjectGalleryFragment) => {
+  const { classes } = useStyles(undefined, { name: 'ProjectGallery' });
+
   return (
-    <>
-      <Text fz="xl" fw="xl" p={40}>
-        Projects
-      </Text>
-      <Flex>
-        <Grid
-          /*  align={"center"} */ mx={{ xs: 10, sm: 50, md: 50, xl: 200 }}
-          /* w={"100vw"} */ justify={'center'}
-          gutter={0}
-          columns={11}
-        >
-          {projects.map((project, i) => (
-            <Grid.Col
-              maw={200}
-              xs={6}
-              sm={5}
-              md={5}
-              lg={3}
-              /* span={3} */ key={i}
-            >
-              <Project key={project.id} {...project} />
-              {i === projects.length - 1 && <LastRow />}
-            </Grid.Col>
-          ))}
-        </Grid>
-      </Flex>
-    </>
-  );
-};
-const LastRow = () => {
-  return (
-    <div style={{ justifyContent: 'flex-start' }}>
-      {/* Your last row content */}
-    </div>
+    <Box className={classes.root}>
+      {projects.map((project, i) => (
+        <Project key={project.id} {...project} />
+      ))}
+    </Box>
   );
 };
 
