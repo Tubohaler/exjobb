062d4453cea1e7841af3c57fd0df9283a1692afd
David Skotte <david.skotte@gmail.com>
2023-01-17 13:13:32 +0100

    styling almost done

--- components/content/Project.tsx
+++ components/content/Project.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Image } from 'react-datocms';
+import SteamButton from '@components/elements/buttons/SteamButton';
 
 import { ProjectFragment } from '@lib/dato-cms';
-import { createStyles, Box, Title, Overlay, Flex, Stack } from '@mantine/core';
+import {
+  createStyles,
+  Box,
+  Title,
+  Overlay,
+  Flex,
+  Stack,
+  Group,
+  Button,
+} from '@mantine/core';
 
 import { createTransition } from '@lib/theme/utils';
 
@@ -20,27 +30,58 @@ const useStyles = createStyles((theme) => ({
     objectFit: 'cover',
     objectPosition: 'center',
   },
-  // title: {},
   overlay: {
     position: 'absolute',
     inset: '0 0 0 0',
     zIndex: 1,
     opacity: 0,
     background: 'rgba(0,0,0,0.5)',
+    color: theme.white,
     transition: createTransition(['opacity']),
     '&:hover': { opacity: 1 },
   },
+  info: {
+    width: '100%',
+    height: '100%',
+  },
+
+  title: {
+    fontWeight: 'normal',
+    // fontSize: '1em',
+    textAlign: 'center',
+  },
+  description: {
+    fontWeight: 'normal',
+    // fontSize: '1em',
+    textAlign: 'center',
+  },
+  buttonGroup: {
+    width: '100%',
+  },
 }));
 
-const Project = ({ name, featuredImage }: ProjectFragment) => {
+const Project = ({ name, featuredImage, description }: ProjectFragment) => {
   const { classes } = useStyles(undefined, { name: 'Project' });
   return (
     <Box className={classes.root}>
-      <Flex className={classes.overlay}>
-        <Stack>
-          <Title order={3}>{name}</Title>
-          <Title></Title>
+      <Flex
+        className={classes.overlay}
+        align="center"
+        justify="center"
+        direction="column"
+      >
+        <Stack className={classes.info} spacing="xs" justify="center">
+          <Title className={classes.title} order={3} size="h4">
+            {name}
+          </Title>
+          <Title className={classes.description} order={4} size="h5" italic>
+            {description}
+          </Title>
         </Stack>
+        <Group className={classes.buttonGroup} grow spacing={0}>
+          <Button>From us</Button>
+          <SteamButton />
+        </Group>
       </Flex>
       <Image className={classes.image} data={featuredImage.responsiveImage} />
     </Box>
