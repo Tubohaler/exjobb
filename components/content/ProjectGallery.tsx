f5d25c6ec689e9d7095c9bdba99a7ed10e9ac0a3
David Skotte <david.skotte@gmail.com>
2023-01-17 10:17:51 +0100

    working on styling the ProjectGallery component

--- components/content/ProjectGallery.tsx
+++ components/content/ProjectGallery.tsx
@@ -2,47 +2,46 @@ import React from 'react';
 
 import Project from './Project';
 
-import { Grid } from '@mantine/core';
+import { Container, Flex, Grid, Text } from '@mantine/core';
 
 import { ProjectFragment, ProjectGalleryFragment } from '@lib/dato-cms';
 
-const sampleProjects: ProjectFragment[] = [
-  {
-    __typename: 'ProjectRecord',
-    id: '90591230',
-    name: 'Book of Travels',
-    pageUrl:
-      'https://mightanddelight.com/bookoftravels?utm_source=mightanddelight_com&utm_medium=projects-page',
-    steamUrl: 'https://store.steampowered.com/app/1152340/Book_of_Travels/',
-    humbleUrl: '',
-    humbleButtonText: 'From us',
-    featuredImage: {
-      __typename: 'ImageFileField',
-      id: '32551773',
-      responsiveImage: {
-        __typename: 'ResponsiveImage',
-        src: 'https://www.datocms-assets.com/91892/1673281258-book-of-travels.jpg?fm=webp',
-        alt: null,
-        title: null,
-        width: 1024,
-        height: 1024,
-        srcSet:
-          'https://www.datocms-assets.com/91892/1673281258-book-of-travels.jpg?dpr=0.25&fm=webp 256w,https://www.datocms-assets.com/91892/1673281258-book-of-travels.jpg?dpr=0.5&fm=webp 512w,https://www.datocms-assets.com/91892/1673281258-book-of-travels.jpg?dpr=0.75&fm=webp 768w,https://www.datocms-assets.com/91892/1673281258-book-of-travels.jpg?fm=webp 1024w',
-        sizes: '(max-width: 512px) 100vw, 512px',
-        bgColor: '#d4c471',
-        aspectRatio: 1,
-      },
-    },
-  },
-];
-
 const ProjectGallery = ({ projects }: ProjectGalleryFragment) => {
   return (
-    <Grid>
-      {projects.map((project) => (
-        <Project key={project.id} {...project} />
-      ))}
-    </Grid>
+    <>
+      <Text fz="xl" fw="xl" p={40}>
+        Projects
+      </Text>
+      <Flex>
+        <Grid
+          /*  align={"center"} */ mx={{ xs: 10, sm: 50, md: 50, xl: 200 }}
+          /* w={"100vw"} */ justify={'center'}
+          gutter={0}
+          columns={11}
+        >
+          {projects.map((project, i) => (
+            <Grid.Col
+              maw={200}
+              xs={6}
+              sm={5}
+              md={5}
+              lg={3}
+              /* span={3} */ key={i}
+            >
+              <Project key={project.id} {...project} />
+              {i === projects.length - 1 && <LastRow />}
+            </Grid.Col>
+          ))}
+        </Grid>
+      </Flex>
+    </>
+  );
+};
+const LastRow = () => {
+  return (
+    <div style={{ justifyContent: 'flex-start' }}>
+      {/* Your last row content */}
+    </div>
   );
 };
 
