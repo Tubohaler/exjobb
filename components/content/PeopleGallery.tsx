709454a366147bb09bff3ae76d724b4e980008e7
David Skotte <david.skotte@gmail.com>
2023-01-20 14:37:15 +0100

    PeopleGallery working, slight styling left

--- components/content/PeopleGallery.tsx
+++ components/content/PeopleGallery.tsx
@@ -7,20 +7,25 @@ import StaffPortrait from './StaffPortrait';
 import { PeopleGalleryFragment } from '@lib/dato-cms';
 
 const useStyles = createStyles((theme) => ({
-  image: {
-    width: '100%',
+  root: {
+    display: 'grid',
+    gridTemplateRows: 'auto',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    alignContent: 'center',
+    justifyItems: 'center',
+
+    [theme.fn.largerThan('md')]: { gridTemplateColumns: 'repeat(3, 1fr)' },
   },
 }));
 
 function PeopleGallery({ people }: PeopleGalleryFragment) {
   const { classes } = useStyles(undefined, { name: 'PeopleGallery' });
   return (
-    <Box>
+    <Box className={classes.root}>
       {people.map((staff) => (
         <StaffPortrait key={staff.id} {...staff} />
       ))}
     </Box>
   );
 }
-
 export default PeopleGallery;
