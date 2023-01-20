019ca71413eae4eb7380968a69294cc63664864c
Vincent Sjögren <vincentsjogren@gmail.com>
2023-01-20 14:29:27 +0100

    working on contact page and article styling

--- components/page/PageSectionHeader.tsx
+++ components/page/PageSectionHeader.tsx
@@ -1,25 +1,29 @@
 import { SectionFragment } from '@lib/dato-cms';
-import { Center, DefaultProps, Selectors, Title } from '@mantine/core';
+import { Box, DefaultProps, Selectors, Title } from '@mantine/core';
 import { createStyles } from '@mantine/styles';
 
 export type PageSectionHeaderProps =
   DefaultProps<PageSectionHeaderStylesNames> &
-    Omit<Parameters<typeof Center<'div'>>[0], 'children'> & {
+    Omit<Parameters<typeof Box<'div'>>[0], 'children'> & {
       title: string;
     };
 
 export type PageSectionHeaderStylesNames = Selectors<typeof useStyles>;
 
-const useStyles = createStyles((theme) => ({
-  root: {
-    width: '100%',
-    marginBottom: theme.spacing.xl * 2,
-  },
-  title: {
-    letterSpacing: '0.1em',
-    textTransform: 'capitalize',
-  },
-}));
+const useStyles = createStyles((theme) => {
+  return {
+    root: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    title: {
+      letterSpacing: '0.1em',
+      textTransform: 'capitalize',
+      textAlign: 'center',
+    },
+  };
+});
 
 const PageSectionHeader = ({
   title,
@@ -36,7 +40,7 @@ const PageSectionHeader = ({
   return !title ? (
     <></>
   ) : (
-    <Center component="div" className={cx(classes.root, className)} {...props}>
+    <Box component="div" className={cx(classes.root, className)} {...props}>
       <Title
         order={2}
         className={classes.title}
@@ -46,7 +50,7 @@ const PageSectionHeader = ({
       >
         {title}
       </Title>
-    </Center>
+    </Box>
   );
 };
 
