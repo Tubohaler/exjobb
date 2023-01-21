3dd6e47e91f0a75f5066e90e9efe2843bc14f845
Vincent Sjögren <vincentsjogren@gmail.com>
2023-01-21 18:41:14 +0100

    updated queries to fetch favicon meta, updated Head to accept query data and render meta tags, and moved Head into PageWrapper

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,24 @@
 import { PageWrapper } from '@components/elements/layout';
 import { createGetStaticPageProps, StaticPageProps } from '@lib/dato-cms';
-import Head from '@components/page/Head';
 import PageSection from '@components/page/PageSection';
 
 export const getStaticProps = createGetStaticPageProps();
 
 const Home = ({ data }: StaticPageProps) => {
   return (
-    <>
-      <Head />
-      <PageWrapper data={data}>
-        {data.page?.sections.map((section) => {
-          return (
-            <PageSection
-              key={section.id}
-              section={section}
-              fullscreen={
-                section.content.links[0]?.__typename === 'ResponsiveVideoRecord'
-              }
-            />
-          );
-        })}
-      </PageWrapper>
-    </>
+    <PageWrapper data={data}>
+      {data.page?.sections.map((section) => {
+        return (
+          <PageSection
+            key={section.id}
+            section={section}
+            fullscreen={
+              section.content.links[0]?.__typename === 'ResponsiveVideoRecord'
+            }
+          />
+        );
+      })}
+    </PageWrapper>
   );
 };
 
