151df7902463b5481fa168dbec829cd57606fbee
Vincent Sjögren <vincentsjogren@gmail.com>
2023-01-23 10:57:37 +0100

    added css animation to body to hide initial unstyled page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@
 import type { AppProps } from 'next/app';
 import mainTheme from '@lib/theme/main';
 import { MantineProvider } from '@mantine/core';
+import '@styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
