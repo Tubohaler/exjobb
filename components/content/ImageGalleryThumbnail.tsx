3289f937484938bdb9746b60848ad774493915a2
Vincent Sjögren <vincentsjogren@gmail.com>
2023-01-23 14:56:48 +0100

    updated styling and removed api folder in pages

--- components/content/ImageGalleryThumbnail.tsx
+++ components/content/ImageGalleryThumbnail.tsx
@@ -5,44 +5,39 @@ import { ImageGalleryFragment } from '@lib/dato-cms';
 import { createStyles, Box, Title } from '@mantine/core';
 import { createTransition } from '@lib/theme/utils';
 
-type ImageGalleryThumbnailStylesParams = { aspectRatio?: number };
-
-const useStyles = createStyles(
-  (theme, { aspectRatio }: ImageGalleryThumbnailStylesParams) => ({
-    root: {
-      aspectRatio: `${aspectRatio || 'auto'}`,
-      width: '100%',
-      height: '100%',
-      position: 'relative',
+const useStyles = createStyles((theme) => ({
+  root: {
+    position: 'relative',
+  },
+  image: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    inset: '0 0 0 0',
+    zIndex: 0,
+    objectFit: 'cover',
+    objectPosition: 'center',
+  },
+  overlay: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    inset: '0 0 0 0',
+    zIndex: 10,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    opacity: 0,
+    background: 'rgba(0,0,0,0.5)',
+    transition: createTransition(['opacity']),
+    '&:hover': {
+      opacity: 1,
     },
-    image: {
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
-      inset: '0 0 0 0',
-      zIndex: 0,
-      objectFit: 'cover',
-      objectPosition: 'center',
-    },
-    overlay: {
-      position: 'absolute',
-      inset: '0 0 0 0',
-      zIndex: 10,
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      opacity: 0,
-      background: 'rgba(0,0,0,0.5)',
-      transition: createTransition(['opacity']),
-      '&:hover': {
-        opacity: 1,
-      },
-    },
-    title: {
-      color: theme.white,
-    },
-  })
-);
+  },
+  title: {
+    color: theme.white,
+  },
+}));
 
 export type ImageGalleryThumbnailProps = {
   image: ImageGalleryFragment['images'][number];
@@ -52,16 +47,13 @@ export type ImageGalleryThumbnailProps = {
 function ImageGalleryThumbnail({
   responsiveImage,
 }: ImageGalleryFragment['images'][number]) {
-  const { classes } = useStyles(
-    { aspectRatio: responsiveImage.aspectRatio },
-    { name: 'Gallery' }
-  );
+  const { classes } = useStyles(undefined, { name: 'Gallery' });
 
   return (
     <Box className={classes.root}>
       {responsiveImage.title && (
         <Box className={classes.overlay}>
-          <Title className={classes.title} italic>
+          <Title className={classes.title} italic order={3}>
             {responsiveImage.title}
           </Title>
         </Box>
--- components/content/StaffPortrait.tsx
+++ components/content/StaffPortrait.tsx
@@ -6,52 +6,45 @@ import { createTransition } from '@lib/theme/utils';
 
 import { StaffFragment } from '@lib/dato-cms';
 
-export type StaffPortraitStylesParams = { aspectRatio?: number };
-
-const useStyles = createStyles(
-  (theme, { aspectRatio }: StaffPortraitStylesParams) => ({
-    root: {
-      width: '100%',
-      height: '100%',
-      aspectRatio: `${aspectRatio || 'auto'}`,
-      position: 'relative',
-    },
-    image: {
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
-      zIndex: 0,
-      objectFit: 'cover',
-      objectPosition: 'center',
-    },
-    overlay: {
-      position: 'absolute',
-      inset: '0 0 0 0',
-      zIndex: 10,
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      opacity: 0,
-      background: 'rgba(0,0,0,0.5)',
-      color: theme.white,
-      transition: createTransition(['opacity']),
-      '&:hover': { opacity: 1 },
-    },
-    name: {
-      textAlign: 'center',
-    },
-    title: {
-      textAlign: 'center',
-    },
-  })
-);
+const useStyles = createStyles((theme) => ({
+  root: {
+    position: 'relative',
+  },
+  image: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    zIndex: 0,
+    objectFit: 'cover',
+    objectPosition: 'center',
+  },
+  overlay: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    zIndex: 10,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    opacity: 0,
+    background: 'rgba(0,0,0,0.5)',
+    color: theme.white,
+    transition: createTransition(['opacity']),
+    '&:hover': { opacity: 1 },
+  },
+  name: {
+    textAlign: 'center',
+    userSelect: 'none',
+  },
+  title: {
+    textAlign: 'center',
+    userSelect: 'none',
+  },
+}));
 
 function StaffPortrait({ featuredImage, name, jobPosition }: StaffFragment) {
-  const { classes } = useStyles(
-    { aspectRatio: featuredImage.responsiveImage.aspectRatio },
-    { name: 'StaffPortrait' }
-  );
+  const { classes } = useStyles(undefined, { name: 'StaffPortrait' });
 
   return (
     <Box className={classes.root}>
