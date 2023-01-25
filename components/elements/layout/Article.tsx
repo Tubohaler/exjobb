c5e8d9c91e3d9c75f8c9309bf8d587f163ff5e6d
Vincent Sjögren <vincentsjogren@gmail.com>
2023-01-25 10:02:04 +0100

    removed unnecessary createStyles in Section and Article components

--- components/elements/layout/Article.tsx
+++ components/elements/layout/Article.tsx
@@ -1,37 +1,14 @@
 import { forwardRef } from 'react';
-import { Box, createStyles, DefaultProps, Selectors } from '@mantine/core';
+import { Box } from '@mantine/core';
 
-export type ArticleStylesNames = Selectors<typeof useStyles>;
-
-export type ArticleProps = DefaultProps<ArticleStylesNames> &
-  Parameters<typeof Box<'article'>>[0] & {
-    children: React.ReactNode;
-  };
-
-const useStyles = createStyles((theme) => ({
-  root: {},
-}));
-
+export type ArticleProps = Parameters<typeof Box<'article'>>[0] & {
+  children: React.ReactNode;
+};
 /**
  * Wraps content with a html article element
  */
-const Article = forwardRef<HTMLDivElement, ArticleProps>(
-  ({ className, classNames, styles, unstyled, ...props }, ref) => {
-    const { classes, cx } = useStyles(undefined, {
-      name: 'Article',
-      classNames,
-      styles,
-      unstyled,
-    });
-    return (
-      <Box
-        ref={ref}
-        component="article"
-        className={cx(classes.root, className)}
-        {...props}
-      />
-    );
-  }
-);
+const Article = forwardRef<HTMLDivElement, ArticleProps>((props, ref) => {
+  return <Box ref={ref} component="article" {...props} />;
+});
 
 export default Article;
--- components/elements/layout/Section.tsx
+++ components/elements/layout/Section.tsx
@@ -1,37 +1,15 @@
-import { Box, createStyles, DefaultProps, Selectors } from '@mantine/core';
+import { Box } from '@mantine/core';
 import { forwardRef } from 'react';
 
-export type SectionStylesNames = Selectors<typeof useStyles>;
-
-export type SectionProps = DefaultProps<SectionStylesNames> &
-  Parameters<typeof Box<'section'>>[0] & {
-    children: React.ReactNode;
-  };
-
-const useStyles = createStyles((theme) => ({
-  root: {},
-}));
+export type SectionProps = Parameters<typeof Box<'section'>>[0] & {
+  children: React.ReactNode;
+};
 
 /**
  * Wraps content with a html section element
  */
-const Section = forwardRef<HTMLDivElement, SectionProps>(
-  ({ className, classNames, styles, unstyled, ...props }, ref) => {
-    const { classes, cx } = useStyles(undefined, {
-      name: 'Section',
-      classNames,
-      styles,
-      unstyled,
-    });
-    return (
-      <Box
-        ref={ref}
-        component="section"
-        className={cx(classes.root, className)}
-        {...props}
-      />
-    );
-  }
-);
+const Section = forwardRef<HTMLDivElement, SectionProps>((props, ref) => {
+  return <Box ref={ref} component="section" {...props} />;
+});
 
 export default Section;
